de72083586fea37becd7c28376450495d3232881
penivaiz <1.4.hoffer@gmail.com>
2021-06-02 22:03:37 +0300

    рабочий run_scraping

--- run_sraping.py
+++ run_sraping.py
@@ -1,9 +1,55 @@
+# import os, sys
+# import datetime as dt
+# import codecs
+#
+# # from django.contrib.auth import get_user_model
+# from django.db import DatabaseError
+#
+# proj = os.path.dirname(os.path.abspath('manage.py'))
+# sys.path.append(proj)
+# os.environ["DJANGO_SETTINGS_MODULE"] = "jobscraper.settings"
+#
+# import django
+# django.setup()
+#
+# from scraping.parsers import work,djinni
+# from scraping.models import Vacancy, City, Language
+#
+# # User = get_user_model()
+#
+# parsers = (
+#     (work, 'https://www.work.ua/jobs-kyiv-python/'),
+#     # (dou, 'dou'),
+#     (djinni, 'https://djinni.co/jobs/location-kyiv'),
+#     # (rabota, 'https://rabota.ua/zapros/python/киев')
+# )
+# jobs, errors = [], []
+#
+# city = City.objects.filter(slug='kiev').first()
+# language = Language.objects.filter(slug='python').first()
+#
+# jobs, errors = [], []
+# for func, url in parsers:
+#     j,e = func(url)
+#     jobs += j
+#     errors += e
+#
+# for job in jobs:
+#     v = Vacancy(**job, city=city, language=language)
+#     v.save()
+#
+# h = codecs.open('work.txt', 'w', 'utf-8')
+# h.write(str(jobs))
+# h.close()
+
+
 import os, sys
 import datetime as dt
 import codecs
 
 # from django.contrib.auth import get_user_model
-from django.db import DatabaseError
+from django.contrib.auth import get_user_model
+from django.db import DatabaseError, IntegrityError
 
 proj = os.path.dirname(os.path.abspath('manage.py'))
 sys.path.append(proj)
@@ -12,58 +58,123 @@ os.environ["DJANGO_SETTINGS_MODULE"] = "jobscraper.settings"
 import django
 django.setup()
 
-from scraping.parsers import *
-from scraping.models import Vacancy, City, Language
+from scraping.parsers import work,djinni
+from scraping.models import Vacancy, City, Language, Url
 
-# User = get_user_model()
+User = get_user_model()
 
 parsers = (
-    # (work, 'work'),
+    (work, 'work'),
     # (dou, 'dou'),
-    (djinni, 'https://djinni.co/jobs/?location=%D0%9A%D0%B8%D0%B5%D0%B2&primary_keyword=Python'),
-    # (rabota, 'rabota')
+    (djinni, 'djinni'),
+    # (rabota, 'https://rabota.ua/zapros/python/киев')
 )
+
+def get_settings():
+    qs = User.objects.filter(send_email=True).values()
+    settings_lst = set((q['city_id'], q['language_id']) for q in qs)
+    return settings_lst
+
+def get_urls(_settings):
+    qs = Url.objects.all().values()
+    url_dict = {(q['city_id'], q['language_id']): q['url_data'] for q in qs}
+    urls = []
+    for pair in _settings:
+        if pair in url_dict:
+            tmp = {}
+            tmp['city'] = pair[0]
+            tmp['language'] = pair[1]
+            url_data = url_dict.get(pair)
+            if url_data:
+                tmp['url_data'] = url_dict.get(pair)
+                urls.append(tmp)
+    return urls
+
+settings = get_settings()
+url_list = get_urls(settings)
+
+
 jobs, errors = [], []
 
-city = City.objects.filter(slug='kiev').first()
-language = Language.objects.filter(slug='python').first()
-
-jobs, errors = [], []
-for func, url in parsers:
-    j,e = func(url)
-    jobs += j
-    errors += e
+for data in url_list:
+    for func, key in parsers:
+        url = data['url_data'][key]
+        j, e = func(url, city=data['city'], language=data['language'])
+        jobs += j
+        errors += e
 
 for job in jobs:
-    v = Vacancy(**job,city=city, language=language)
+    v = Vacancy(**job)
     v.save()
+    try:
+        v.save()
+    except DatabaseError:
+        pass
+    except IntegrityError:
+        pass
 
-h = codecs.open('work.txt1', 'w', 'utf-8')
-h.write(str(jobs))
-h.close()
+
+# h = codecs.open('work.txt', 'w', 'utf-8')
+# h.write(str(jobs))
+# h.close()
 
 
 
+
+
+#
+#
+# import asyncio
+# import codecs
+# import os, sys
+# import datetime as dt
+#
+# from django.contrib.auth import get_user_model
+# from django.db import DatabaseError
+#
+# proj = os.path.dirname(os.path.abspath('manage.py'))
+# sys.path.append(proj)
+# os.environ["DJANGO_SETTINGS_MODULE"] = "jobscraper.settings"
+#
+# import django
+# django.setup()
+#
+# from scraping.parsers import *
+# from scraping.models import Vacancy, Error, Url
+#
+# User = get_user_model()
+#
+# parsers = (
+#     (work, 'work'),
+#     (dou, 'dou'),
+#     (djinni, 'djinni'),
+#     (rabota, 'rabota')
+# )
+# jobs, errors = [], []
+#
+#
 # def get_settings():
 #     qs = User.objects.filter(send_email=True).values()
 #     settings_lst = set((q['city_id'], q['language_id']) for q in qs)
 #     return settings_lst
-
-
+#
+#
 # def get_urls(_settings):
 #     qs = Url.objects.all().values()
-#     url_dct = {(q['city_id'], q['language_id']): q['url_data'] for q in qs}
+#     url_dict = {(q['city_id'], q['language_id']): q['url_data'] for q in qs}
 #     urls = []
 #     for pair in _settings:
-#         if pair in url_dct:
+#         if pair in url_dict:
 #             tmp = {}
 #             tmp['city'] = pair[0]
 #             tmp['language'] = pair[1]
-#             tmp['url_data'] = url_dct[pair]
-#             urls.append(tmp)
+#             url_data = url_dict.get(pair)
+#             if url_data:
+#                 tmp['url_data'] = url_dict.get(pair)
+#                 urls.append(tmp)
 #     return urls
 #
-
+#
 # async def main(value):
 #     func, url, city, language = value
 #     job, err = await loop.run_in_executor(None, func, url, city, language)
@@ -77,19 +188,19 @@ h.close()
 # tmp_tasks = [(func, data['url_data'][key], data['city'], data['language'])
 #              for data in url_list
 #              for func, key in parsers]
-
-# for data in url_list:
 #
-#     for func, key in parsers:
-#         url = data['url_data'][key]
-#         j, e = func(url, city=data['city'], language=data['language'])
-#         jobs += j
-#         errors += e
+# # for data in url_list:
+# #
+# #     for func, key in parsers:
+# #         url = data['url_data'][key]
+# #         j, e = func(url, city=data['city'], language=data['language'])
+# #         jobs += j
+# #         errors += e
 # if tmp_tasks:
 #     tasks = asyncio.wait([loop.create_task(main(f)) for f in tmp_tasks])
 #     loop.run_until_complete(tasks)
 #     loop.close()
-
+#
 # for job in jobs:
 #     v = Vacancy(**job)
 #     try:
@@ -104,7 +215,9 @@ h.close()
 #         err.save()
 #     else:
 #         er = Error(data=f'errors:{errors}').save()
-
-# h = codecs.open('work.txt1', 'w', 'utf-8')
+#
+# h = codecs.open('work.txt', 'w', 'utf-8')
 # h.write(str(jobs))
 # h.close()
+# # ten_days_ago = dt.date.today() - dt.timedelta(10)
+# # Vacancy.objects.filter(timestamp__lte=ten_days_ago).delete()
--- scraping/parsers.py
+++ scraping/parsers.py
@@ -3,7 +3,7 @@ import codecs
 from bs4 import BeautifulSoup as BS
 from random import randint
 
-__all__ = ('work', "rabota", 'dou', 'djinni')
+__all__ = ('work', 'djinni')
 
 headers = [
     {'User-Agent': 'Mozilla/5.0 (Windows NT 5.1; rv:47.0) Gecko/20100101 Firefox/47.0',
@@ -45,83 +45,83 @@ def work(url, city=None, language=None):
     return jobs, errors
 
 
-def rabota(url, city=None, language=None):
-    jobs = []
-    errors = []
-    domain = 'https://rabota.ua'
-    if url:
-        resp = requests.get(url, headers=headers[randint(0, 2)])
-        if resp.status_code == 200:
-            soup = BS(resp.content, 'html.parser')
-            new_jobs = soup.find('div',
-                                 attrs={'class': 'f-vacancylist-newnotfound'})
-            if not new_jobs:
-                table = soup.find('table',
-                                  id='ctl00_content_vacancyList_gridList')
-                if table:
-                    tr_lst = table.find_all('tr', attrs={'id': True})
-                    for tr in tr_lst:
-                        div = tr.find('div',  attrs={'class': 'card-body'})
-                        if div:
-                            title = div.find('p',
-                                             attrs={'class': 'card-title'})
-                            href = title.a['href']
-                            content = div.p.text
-                            company = 'No name'
-                            p = div.find('p', attrs={'class': 'company-name'})
-                            if p:
-                                company = p.a.text
-                            jobs.append({
-                                'title': title.text,
-                                 'url': domain + href,
-                                 'description': content,
-                                 'company': company,
-                                 'city_id': city, 'language_id': language})
-                else:
-                    errors.append({'url': url, 'title': "Table does not exists"})
-            else:
-                errors.append({'url': url, 'title': "Page is empty"})
-        else:
-            errors.append({'url': url, 'title': "Page do not response"})
+# def rabota(url, city=None, language=None):
+#     jobs = []
+#     errors = []
+#     domain = 'https://rabota.ua'
+#     if url:
+#         resp = requests.get(url, headers=headers[randint(0, 2)])
+#         if resp.status_code == 200:
+#             soup = BS(resp.content, 'html.parser')
+#             new_jobs = soup.find('div',
+#                                  attrs={'class': 'f-vacancylist-newnotfound'})
+#             if not new_jobs:
+#                 table = soup.find('table',
+#                                   id='ctl00_content_vacancyList_gridList')
+#                 if table:
+#                     tr_lst = table.find_all('tr', attrs={'id': True})
+#                     for tr in tr_lst:
+#                         div = tr.find('div',  attrs={'class': 'card-body'})
+#                         if div:
+#                             title = div.find('p',
+#                                              attrs={'class': 'card-title'})
+#                             href = title.a['href']
+#                             content = div.p.text
+#                             company = 'No name'
+#                             p = div.find('p', attrs={'class': 'company-name'})
+#                             if p:
+#                                 company = p.a.text
+#                             jobs.append({
+#                                 'title': title.text,
+#                                  'url': domain + href,
+#                                  'description': content,
+#                                  'company': company,
+#                                  'city_id': city, 'language_id': language})
+#                 else:
+#                     errors.append({'url': url, 'title': "Table does not exists"})
+#             else:
+#                 errors.append({'url': url, 'title': "Page is empty"})
+#         else:
+#             errors.append({'url': url, 'title': "Page do not response"})
+#
+#     return jobs, errors
 
-    return jobs, errors
 
-
-def dou(url, city=None, language=None):
-    jobs = []
-    errors = []
-    # domain = 'https://www.work.ua'
-    if url:
-        resp = requests.get(url, headers=headers[randint(0, 2)])
-        if resp.status_code == 200:
-            soup = BS(resp.content, 'html.parser')
-            main_div = soup.find('div', id='vacancyListId')
-            if main_div:
-                li_lst = main_div.find_all('li', attrs={'class': 'l-vacancy'})
-                for li in li_lst:
-                    title = li.find('div', attrs={'class': 'title'})
-                    href = title.a['href']
-                    cont = li.find('div', attrs={'class': 'sh-info'})
-                    content = cont.text
-                    company = 'No name'
-                    a = title.find('a', attrs={'class': 'company'})
-                    if a:
-                        company = a.text
-                    jobs.append({'title': title.text, 'url': href,
-                                 'description': content, 'company': company,
-                                 'city_id': city, 'language_id': language})
-            else:
-                errors.append({'url': url, 'title': "Div does not exists"})
-        else:
-            errors.append({'url': url, 'title': "Page do not response"})
-
-    return jobs, errors
+# def dou(url, city=None, language=None):
+#     jobs = []
+#     errors = []
+#     domain = 'https://www.work.ua'
+#     if url:
+#         resp = requests.get(url, headers=headers[randint(0, 2)])
+#         if resp.status_code == 200:
+#             soup = BS(resp.content, 'html.parser')
+#             main_div = soup.find('div', id='vacancyListId')
+#             if main_div:
+#                 li_lst = main_div.find_all('li', attrs={'class': 'l-vacancy'})
+#                 for li in li_lst:
+#                     title = li.find('div', attrs={'class': 'title'})
+#                     href = title.a['href']
+#                     cont = li.find('div', attrs={'class': 'sh-info'})
+#                     content = cont.text
+#                     company = 'No name'
+#                     a = title.find('a', attrs={'class': 'company'})
+#                     if a:
+#                         company = a.text
+#                     jobs.append({'title': title.text, 'url': href,
+#                                  'description': content, 'company': company,
+#                                  'city_id': city, 'language_id': language})
+#             else:
+#                 errors.append({'url': url, 'title': "Div does not exists"})
+#         else:
+#             errors.append({'url': url, 'title': "Page do not response"})
+#
+#     return jobs, errors
 
 
 def djinni(url, city=None, language=None):
     jobs = []
     errors = []
-    domain = 'https://djinni.co/jobs/?location=%D0%9A%D0%B8%D0%B5%D0%B2&primary_keyword=Python'
+    domain = 'https://djinni.co'
     if url:
         resp = requests.get(url, headers=headers[randint(0, 2)])
         if resp.status_code == 200:
@@ -143,8 +143,8 @@ def djinni(url, city=None, language=None):
                     if comp:
                         company = comp.text
                     jobs.append({'title': title.text, 'url': domain + href,
-                                 'description': content, 'company': company,})
-                                 # 'city_id': city, 'language_id': language})
+                                 'description': content, 'company': company,
+                                 'city_id': city, 'language_id': language})
             else:
                 errors.append({'url': url, 'title': "Div does not exists"})
         else:
